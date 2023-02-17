b93cc529be16a78a7246774dcc1e45daa3c4e539
Aria-ista-0819 <aalva129@calpoly.edu>
2023-02-16 16:52:36 -0800

    camera follow + build setting changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayButton(){
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Game");
     }
 
     public void QuitButton(){
