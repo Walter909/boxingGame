83a11f5dae6c595e254383c398431acf4a74408a
Aria-ista-0819 <aalva129@calpoly.edu>
2023-02-20 16:56:31 -0800

    added background + changed cameraFollow script

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+    public GameObject player;
+    private Vector3 offset;
 
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        //set position
-        transform.position = target.position + offset;
+        transform.position = player.transform.position + offset;
     }
 }
